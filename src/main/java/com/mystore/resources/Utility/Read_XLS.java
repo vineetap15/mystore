ff95174136736739ab17c9a420853e1f583f2054
vineetap15 <vineetapandey24194@gmail.com>
2018-02-10 19:57:02 +0530

    add utility file

---
+++ src/main/java/com/mystore/resources/Utility/Read_XLS.java
@@ -0,0 +1,10 @@
+package com.mystore.resources.Utility;
+
+public class Read_XLS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/mystore/resources/Utility/SuitUtility.java
@@ -0,0 +1,10 @@
+package com.mystore.resources.Utility;
+
+public class SuitUtility {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
